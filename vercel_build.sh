bcf339872df3b06fae6c0dea711e6d3da749f47e
Mishita0823 <mishitatiwari27@gmail.com>
2026-05-30 20:26:32 +0530

    Fix Vercel Flutter deployment

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Clone Flutter 3.41.6
-git clone -b 3.41.6 https://github.com/flutter/flutter.git --depth 1
-export PATH="$PWD/flutter/bin:$PATH"
+set -e
 
-# Enable web support
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+
+export PATH="$PATH:`pwd`/flutter/bin"
+
 flutter config --enable-web
 
-# Go to app folder
 cd qr_utility_app
 
-# Get dependencies
 flutter pub get
 
-# Build Flutter Web
 flutter build web --release
